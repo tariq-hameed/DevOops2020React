84e530ed72ff7ab83880de49a37a7c9f46cdcaec
Tariq Hameed <tariq.hameed@utb.ecutbildning.se>
2020-11-18 16:06:51 +0100

    new things in signin page

--- src/components/navigationbar/NavigationBar.js
+++ src/components/navigationbar/NavigationBar.js
@@ -1,11 +1,15 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import Logotype from '../../shared/Images/logotype.svg'
 import {useHistory} from 'react-router-dom'
 import RoutinPath from '../../routes/RoutingPath'
+import {UserContext} from '../../shared/provider/UserProvider'
 import './NavigationBar.css'
 
 export const NavigationBar = () => {
     const history = useHistory()
+    const [authenticatedUser] = useContext(UserContext)
+
+
     return (
         <div className="navigationBarWrapper">
          
@@ -14,7 +18,12 @@ export const NavigationBar = () => {
                 className="logotype"
                 src = {Logotype} 
                 alt ={"Error .."} />
-                <span className="signIn" onClick={()=> history.push(RoutinPath.SignInView)}>Sign In</span>
+            <span className="signIn" 
+                onClick={()=> history.push(RoutinPath.SignInView)}>Sign In
+            </span>
+            <span>
+                {authenticatedUser}
+            </span>
         </div>
     )
 }
--- src/views/SignInView.js
+++ src/views/SignInView.js
@@ -1,17 +1,27 @@
-import React, { useState} from 'react'
+import React, { useState, useContext} from 'react'
+import {UserContext} from '../shared/provider/UserProvider'
 
 export const SignInView = () => {
-    const [username, setUsername] = useState("Arasto")
-    const [number, setNumber] = useState(10)
+    
+    const [username, setUsername] = useState()
+    const [authenticatedUser, setAuthenticatedUser] = useContext(UserContext)
+
+    const login = (e) => {
+        e.preventDefault()
+        setAuthenticatedUser(username)
+    }
+
     return(
         <div>
-            <h1>{username}</h1>
-            <button onClick={() => setUsername("Tariq")}>Update the value of the username state</button>
-            <hr /><br />
 
-            <h1>{number}</h1>
-            <button onClick={()=>setNumber - 1}>Decrement</button>
-            <button onClick={()=>setNumber + 1}>Increment</button>
+            <form>
+                <span>UserName :</span> 
+                <input onChange={(event) => setUsername(event.target.value)} /> <br />
+                <span>Password: </span>
+                <input /> <br />
+                <button onClick={(e) => login(e)}>LogIn</button>
+            </form>
+          
 
         </div>
     )
