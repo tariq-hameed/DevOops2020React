05e57e41d26875688c428cfde59b70c8a3702cb5
Tariq Hameed <tariq.hameed@utb.ecutbildning.se>
2020-11-18 14:33:52 +0100

    sign in page

--- src/components/navigationbar/NavigationBar.js
+++ src/components/navigationbar/NavigationBar.js
@@ -1,13 +1,19 @@
 import React from 'react'
 import Logotype from '../../shared/Images/logotype.svg'
+import {useHistory} from 'react-router-dom'
 import './NavigationBar.css'
 
 export const NavigationBar = () => {
+    const history = useHistory()
     return (
         <div className="navigationBarWrapper">
          
-            <img onClick={() => alert("you clicked on the image")}
-             className="logotype" src = {Logotype} alt ={"Error .."} />
+            <img 
+                onClick={() => alert("you clicked on the image")}
+                className="logotype"
+                src = {Logotype} 
+                alt ={"Error .."} />
+                <span className="signIn" onClick={()=> history.push('/signin')}>Sign In</span>
         </div>
     )
 }
--- src/routes/Routes.js
+++ src/routes/Routes.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { BrowserRouter as Router, Switch, Route} from 'react-router-dom'
 import { HomeView } from '../views/HomeView'
 import { PageNotFoundView } from '../views/PageNotFoundView'
+import {SignInView} from '../views/SignInView'
 
 export const Routes = (props) => {
 
@@ -10,6 +11,7 @@ export const Routes = (props) => {
             {props.children}
             <Switch>
                 <Route exact path='/home' component={HomeView} />
+                <Route exact path='/signin' component={SignInView} />
                 <Route component={PageNotFoundView} />
             </Switch>
         </Router>
